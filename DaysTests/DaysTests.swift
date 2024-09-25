2dbd78691feb0de8e98971ed881a209a2392c52c
Manuel <mannybauti@gmail.com>
2024-09-25 10:35:20 -0400

    Initial Commit

---
+++ DaysTests/DaysTests.swift
@@ -0,0 +1,16 @@
+//
+//  DaysTests.swift
+//  DaysTests
+//
+//  Created by Manuel Bautista on 9/25/24.
+//
+
+import Testing
+
+struct DaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
